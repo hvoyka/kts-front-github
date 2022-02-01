faaf04cf52c6899930fffeb989e3aeed522f8dd3
Lavrentev Stanislav <hvoinii@gmail.com>
2022-02-01 19:55:53 +0600

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import 'assets/styles/reset.css';
+import 'assets/styles/global.css';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
